a95865ec4c6d1da04bcc2e8e7798a03afcb4282c
djoun52 <simon.rioux.trv@gmail.com>
2022-08-01 12:01:57 +0200

    requeste and response done

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Delete, Get, Post, Put, Req, Res } from "@nestjs/common";
+import { Request, Response } from "express";
+
+@Controller("todo")
+export class TodoController {
+  @Get()
+  getTodos() {
+    console.log("Récupérer la liste de todos");
+    return "Liste des TODOS";
+  }
+
+  @Get("V2")
+  getTodosV2(
+		@Req() request: Request, 
+		@Res() response: Response
+	){
+    console.log("Récupérer la liste de todos");
+    response.status(205);
+    response.json({
+      contenu: `je suis une réponse généré à partir de l'objet Response de express `
+    });
+  }
+
+  @Post()
+  addTodo() {
+    console.log("Ajouter un Todo à la liste de todos");
+    return "Add TODO";
+  }
+
+  @Delete()
+  deleteTodo() {
+    console.log("Suprimer un Todo de la liste de todos");
+    return "Delete TODO";
+  }
+  @Put()
+  modifierTodo() {
+    console.log("Modifier un Todo de la liste de todos");
+    return "Update TODO";
+  }
+}
